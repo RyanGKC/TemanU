aa1cb90b7c81083c6c190e7e73145475a29b33ba
zy2045 <zy2045@hw.ac.uk>
2026-02-09 22:29:13 +0800

    added contents to homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,15 +1,48 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Home Page",
-        style: TextStyle(fontSize: 32),
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: false,
+        title: Text(
+          'Hi, James',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w600,
+            color: Color(0xff00E5FF),
+          )
+        ),
+        flexibleSpace: ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40), // Controls blur intensity
+            child: Container(
+              color: Colors.white.withValues(alpha: 0.25),
+            )
+          ),
+        ),
       ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Color(0xff040F31)
+            )
+          )
+        ]
+      )
     );
   }
 }
